103b88fd75b98330accb77e6ed6646055b57fe74
Rafaynmal <rafihayyan16@gmail.com>
2024-11-04 22:12:51 +0700

    Add files via upload

---
+++ GantiKata.java
@@ -0,0 +1,33 @@
+package pertemuan6;
+
+/**
+ *
+ * @author Afra Syavina
+ */
+import java.util.Scanner;
+
+public class GantiKata {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input kalimat dari user
+        System.out.print("Masukkan Kalimat: ");
+        String kalimat = scanner.nextLine();
+
+        // Input kata yang akan diganti
+        System.out.print("Ganti Kata: ");
+        String gantiKata = scanner.nextLine();
+
+        // Input kata pengganti
+        System.out.print("Menjadi Kata: ");
+        String menjadiKata = scanner.nextLine();
+
+        // Mengganti kata dalam kalimat
+        String kalimatBaru = kalimat.replace(gantiKata, menjadiKata);
+
+        // Output hasil perubahan
+        System.out.println("\nHasil Perubahannya:");
+        System.out.println(kalimatBaru);
+    }
+}
+
